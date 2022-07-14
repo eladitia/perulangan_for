078edb96f9a66678a53815911af873d5d9e91f62
Elki Aditia Anugrah <107825787+eladitia@users.noreply.github.com>
2022-07-14 11:12:15 +0700

    perulangan_for
    materi perulangan for

---
+++ perulangan.py
@@ -0,0 +1,64 @@
+# angka = 1
+
+# for i in range(10) :
+#    print('N:', i, ' berisi:', angka)
+
+
+
+
+# Membuat array buh-buahan
+# Jumlah Index 6
+# Looping menggunakan for
+
+# buah = ['mangga', 'apel', 'anggur', 'jeruk', 'pisang', 'nanas', 'ceri'] 
+
+# # melakukan perulangan for
+# for buah_buahan in buah:
+#     # mencetak buah-buahan
+#     print("Nama buah : ", buah_buahan)
+
+
+
+
+# # Membuat Program Biodata
+
+# # Membuat Variabel nama_saya yang menerima inputan
+# nama_lengkap = input("Masukkan Nama Lengkap : ")
+
+# # Membuat Variabel asal_daerah yang menerima inputan
+# asal_daerah = input("Masukkan Asal Daerah ")
+
+# # Membuat array biodata untuk mengumpulkan hasil inputan
+# biodata = [ nama_lengkap, asal_daerah, ]
+
+# no = 0
+# for isi_biodata in biodata :
+#     print(biodata[no])
+#     no = no + 1
+
+
+
+
+# # membuat array siswa
+# siswa = []
+
+# # membuat perulangan for
+# for i in range(3):
+
+#     # mengecek nilai indeks
+#     print("")
+#     print("ini adalah indeks ke-",i)
+
+#     # menerima inputan nama
+#     nama_siswa = input("masukkan nama siswa : ")
+
+#     # memasukkan hasil inputan nama ke array siswa
+#     siswa.append(nama_siswa)
+
+# print("")
+
+# # membuat perulangan untuk mencetak data dari array siswa
+# for k in siswa:
+
+#     # mencetak array siswa
+#     print("Nama Siswa : " , k)
